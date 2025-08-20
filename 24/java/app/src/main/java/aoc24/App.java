773d81f93f6f2037f432ce59fb91dd5288ed20bd
Chufretalas <101844490+Chufretalas@users.noreply.github.com>
2025-08-20 09:46:42 -0300

    done d8 24 pt2

--- 24/java/app/src/main/java/aoc24/App.java
+++ 24/java/app/src/main/java/aoc24/App.java
@@ -3,6 +3,6 @@ package aoc24;
 
 public class App {
     public static void main(String[] args) {
-        D8.part1();
+        D8.part2();
     }
 }
--- 24/java/app/src/main/java/aoc24/D8.java
+++ 24/java/app/src/main/java/aoc24/D8.java
@@ -9,7 +9,7 @@ record Node(int x, int y, char nodeType) {
 }
 
 public class D8 {
-    static public void part1() {
+    public static void part1() {
         ArrayList<Node> antennas = new ArrayList<>();
         Set<Node> antinodes = new HashSet<Node>();
         int maxX = 0;
@@ -55,6 +55,79 @@ public class D8 {
             }
         }
 
-        System.err.println(antinodes.size());
+        System.out.println(antinodes.size());
+    }
+
+    public static void part2() {
+        ArrayList<Node> antennas = new ArrayList<>();
+        Set<Node> antinodes = new HashSet<Node>();
+        int maxX = 0;
+        int maxY = 0;
+
+        try (LineReader reader = new LineReader("d8.txt")) {
+            Optional<String> lineOption = reader.nextLine();
+
+            while (lineOption.isPresent()) {
+                String line = lineOption.get();
+                maxX = line.length();
+                maxY++;
+                char[] chars = line.toCharArray();
+                for (int i = 0; i < chars.length; i++) {
+                    if (chars[i] != '.') {
+                        antennas.add(new Node(i, maxY - 1, chars[i]));
+                    }
+                }
+                lineOption = reader.nextLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        for (Node currentNode : antennas) {
+            for (Node otherNode : antennas) {
+                if (currentNode.equals(otherNode) || currentNode.nodeType() != otherNode.nodeType())
+                    continue;
+
+                // getting the vector from one antenna to another
+                int vX = currentNode.x() - otherNode.x();
+                int vY = currentNode.y() - otherNode.y();
+
+                // simplify the vector to it's minimal whole value
+                int divisor = Math.abs(vX) < Math.abs(vY) ? Math.abs(vX) : Math.abs(vY);
+                if (divisor != 0) {
+                    while (divisor > 1 && !(vX % divisor == 0 && vY % divisor == 0)) {
+                        divisor--;
+                    }
+
+                    vX /= divisor;
+                    vY /= divisor;
+                }
+
+                // use the vector to walk forwads and backwards starting from one antenna
+                int newAntiX = currentNode.x();
+                int newAntiY = currentNode.y();
+
+                // forwards
+                while (newAntiX >= 0 && newAntiX < maxX && newAntiY >= 0 && newAntiY < maxY) {
+                    antinodes.add(new Node(newAntiX, newAntiY, '#'));
+                    newAntiX += vX;
+                    newAntiY += vY;
+                }
+
+                // resetting the new antinode positions
+                newAntiX = currentNode.x();
+                newAntiY = currentNode.y();
+
+                // backwards
+                while (newAntiX >= 0 && newAntiX < maxX && newAntiY >= 0 && newAntiY < maxY) {
+                    antinodes.add(new Node(newAntiX, newAntiY, '#'));
+                    newAntiX -= vX;
+                    newAntiY -= vY;
+                }
+            }
+        }
+
+        System.out.println(antinodes.size());
     }
 }
